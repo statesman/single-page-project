2ae17d8f38a27b86adddd9632edc7047d3ab686d
critmcdonald <christian@cmcdonald.com>
2016-08-05 13:38:21 -0500

    removing unneeded loadNpmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -184,8 +184,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-ftpush');
   grunt.loadNpmTasks('grunt-bootlint');
 
